2f7bfa0e3260dac5d1de5ed339461273c8435f32
tran thanh quang <quang.tran290699@gmail.com>
2020-08-31 21:46:04 +0700

    add socket lib

---
+++ server/server.js
@@ -0,0 +1,34 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+const port = 3456;
+
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+io.on('connection', () => {
+  console.log('a user is connected');
+});
+
+const messages = [];
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/messages', (req, res) => {
+  res.send(messages);
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+
+app.post('/messages', (req, res) => {
+  messages.push(req.body);
+  io.emit('message', req.body);
+  res.send(messages);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
