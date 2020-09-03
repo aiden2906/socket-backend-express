92b7801e13a8ff275c638e5f3a0f8aa3c1d2e316
tran thanh quang <quang.tran290699@gmail.com>
2020-09-03 21:02:42 +0700

    update connectionString postgresql

--- server/server.js
+++ server/server.js
@@ -3,15 +3,13 @@ const express = require('express');
 const { Client } = require('pg');
 const { create_database, create_table } = require('./query');
 
-
-const connectionString = `postgresql://nodejs:123456@localhost:5432/socketexpress`;
+const connectionString = `postgresql://nodejs:123456@db:5432/socketexpress`;
 const client = new Client({
   connectionString: connectionString,
 });
 
 const app = express();
 const port = 3456;
-const messages = [];
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
@@ -29,7 +27,7 @@ async function connect() {
 (async function server() {
   await connect();
   app.get('/message', (req, res) => {
-    res.send(messages);
+    client.query(`SELECT * FROM message;`).then((resp) => res.send(resp.rows));
   });
 
   app.get('/', (req, res) => {
@@ -37,13 +35,16 @@ async function connect() {
   });
 
   app.post('/message', (req, res) => {
-    console.log(req.body.message);
-    messages.push(req.body.message);
-    client.query(`INSERT INTO message VALUES (2,2,${req.body.message});`).then(res=> console.log(res)).catch(err=>console.log(err)).finally(()=>{
-      client.end();
-    })
-    io.emit('message', req.body);
-    res.send(messages);
+    client
+      .query(`INSERT INTO message VALUES (2,2,'${req.body.message}');`)
+      .then((res) => {
+        io.emit('message', req.body);
+        res.send(messages);
+      })
+      .catch((err) => console.log(err))
+      .finally(() => {
+        client.end();
+      });
   });
 
   io.on('connection', () => {
